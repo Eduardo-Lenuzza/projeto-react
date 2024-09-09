3832a289b0696bb439af09aacd585917c961a0bd
dudulenuzza@hotmail.com <dudulenuzza@hotmail.com>
2024-09-09 18:27:37 -0300

    Inicializando repositório atividade front-end

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <nav className="menu-bar">
+        <ul>
+          <li>
+            <a href="#blog">Blog</a>
+          </li>
+          <li>
+            <a href="#sobre">Sobre</a>
+          </li>
+          <li>
+            <a href="#descricao">Descrição</a>
+          </li>
+        </ul>
+      </nav>
+      <header className="header">
+        <div className="image-column">
+          <img
+            src="https://st2.depositphotos.com/1146092/9591/i/600/depositphotos_95915460-stock-photo-smart-dog-isolated-on-black.jpg"
+            alt="Seu nome"
+            className="profile-image"
+          />
+        </div>
+        <div className="text-column">
+          <h1>Seu nome</h1>
+          <p>
+            Coloque aqui sua biografia de forma resumida. Coloque aqui sua
+            biografia de forma resumida. Coloque aqui sua biografia de forma
+            resumida. Coloque aqui sua biografia de forma resumida.
+          </p>
+        </div>
       </header>
+      <div className="blog-column">
+        <section className="blog-section">
+          <h2>Últimas do blog</h2>
+          <ul>
+            <li>
+              <a href="/">5 dicas para sua carreira profissional</a>
+              <p>
+                Confira algumas dicas que podem ajudar a alavancar sua carreira
+                como desenvolvedor front-end
+              </p>
+            </li>
+            <li>
+              <a href="/">5 dicas para sua carreira profissional</a>
+              <p>
+                Confira algumas dicas que podem ajudar a alavancar sua carreira
+                como desenvolvedor front-end
+              </p>
+            </li>
+            <li>
+              <a href="/">5 dicas para sua carreira profissional</a>
+              <p>
+                Confira algumas dicas que podem ajudar a alavancar sua carreira
+                como desenvolvedor front-end
+              </p>
+            </li>
+          </ul>
+          <a href="/" className="see-all">
+            Ver tudo
+          </a>
+        </section>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
